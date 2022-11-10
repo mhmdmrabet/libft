0ee0f70c4be484a8b6c7200c4f7495476b3ebc11
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2022-11-10 20:28:57 +0100

    ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mmrabet <mmrabet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:14:00 by mmrabet           #+#    #+#             */
-/*   Updated: 2022/11/08 09:04:56 by mmrabet          ###   ########.fr       */
+/*   Updated: 2022/11/10 20:22:36 by mmrabet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	(void)n;
-	(void)fd;
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + 48, fd);
 }
